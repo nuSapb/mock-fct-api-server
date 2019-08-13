6c48f32565d583fef02a52c0b339ec6d5a4f37f6
nuhundred@gmail.com <nuhundred@gmail.com>
2019-08-13 14:31:58 +0700

    create project structure

---
+++ src/config/db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `mock_test_db`.`snn4k_91002016`
+( `serial_number` VARCHAR
+(50) NOT NULL , `part_number` VARCHAR
+(50) NOT NULL , `rev` VARCHAR
+(50) NOT NULL , `test_step` INT NOT NULL , `description` VARCHAR
+(150) NOT NULL , `measurement_value` INT NULL , `limit_min` INT NOT NULL , `limit_max` INT NOT NULL , `result` INT NOT NULL , `test_user` INT NOT NULL ) ENGINE = InnoDB;
